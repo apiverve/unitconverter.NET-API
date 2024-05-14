ed739161e43883414a96bac6289db37fcfefff4a
APIVerve Team <hello@apiverve.com>
2024-05-14 15:44:15 -0500

    Bug fixes and Improvements

--- ResponseObj.cs
+++ ResponseObj.cs
@@ -1,21 +1,90 @@
 using System;
 using System.Collections.Generic;
-using System.Text;
+using Newtonsoft.Json;
 
 namespace APIVerve
 {
-    using Newtonsoft.Json;
+public class result
+{
+    [JsonProperty("result")]
+    public double result { get; set; }
+
+    [JsonProperty("from")]
+    public string from { get; set; }
+
+    [JsonProperty("to")]
+    public string to { get; set; }
+
+}
+
+public class from
+{
+    [JsonProperty("abbr")]
+    public string abbr { get; set; }
+
+    [JsonProperty("measure")]
+    public string measure { get; set; }
+
+    [JsonProperty("system")]
+    public string system { get; set; }
+
+    [JsonProperty("singular")]
+    public string singular { get; set; }
+
+    [JsonProperty("plural")]
+    public string plural { get; set; }
+
+}
+
+public class to
+{
+    [JsonProperty("abbr")]
+    public string abbr { get; set; }
+
+    [JsonProperty("measure")]
+    public string measure { get; set; }
+
+    [JsonProperty("system")]
+    public string system { get; set; }
+
+    [JsonProperty("singular")]
+    public string singular { get; set; }
+
+    [JsonProperty("plural")]
+    public string plural { get; set; }
+
+}
+
+public class unitDefinitions
+{
+    [JsonProperty("from")]
+    public from from { get; set; }
+
+    [JsonProperty("to")]
+    public to to { get; set; }
+
+}
+
+public class data
+{
+    [JsonProperty("result")]
+    public result result { get; set; }
+
+    [JsonProperty("unitDefinitions")]
+    public unitDefinitions unitDefinitions { get; set; }
+
+}
 
 public class ResponseObj
 {
     [JsonProperty("status")]
-    public string Status { get; set; }
+    public string status { get; set; }
 
     [JsonProperty("error")]
-    public string Error { get; set; }
+    public object error { get; set; }
 
     [JsonProperty("data")]
-    public object Data { get; set; }
+    public data data { get; set; }
 
 }
 
