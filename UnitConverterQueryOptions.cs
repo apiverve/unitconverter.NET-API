0533edec3f7ab985bc5521b5396fad3d4002e1a9
APIVerve Team <hello@apiverve.com>
2024-08-20 11:12:42 -0500

    Bug Fixes

---
+++ UnitConverterQueryOptions.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class UnitConverterQueryOptions {
+public double value { get; set; }
+public string from { get; set; }
+public string to { get; set; }
+}
+
+}
